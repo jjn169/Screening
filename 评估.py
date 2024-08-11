f004d69445c32ae83a1b7a09b5c227dee5ea84ab
jjn169 <915683117@qq.com>
2024-08-11 13:42:58 +0800

    Update 评估.py

--- 评估.py
+++ 评估.py
@@ -4,12 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import joblib
 import pandas as pd
+GITHUB_USER = 'jjn169'
+REPO_NAME = 'Screening'
+TAG_NAME = 'rf'  # 这是你的Tag名称
+FILE_NAME = 'RandomForest.joblib'
 
+# 构建GitHub文件下载URL
+url = f'https://github.com/{GITHUB_USER}/{REPO_NAME}/raw/{TAG_NAME}/{FILE_NAME}'
+
+# 下载文件
+response = requests.get(url)
+if response.status_code == 200:
+    # 将文件保存到本地临时目录
+    with open(FILE_NAME, 'wb') as f:
+        f.write(response.content)
+    # 加载模型文件
+    model = joblib.load(FILE_NAME)
+    os.remove(FILE_NAME)  # 加载后删除文件
+else:
+    print(f"Failed to download file: {response.status_code}")
 # 加载SHAP值
 shap_values = np.load("shap1.npy", allow_pickle=True)
 
-# 加载模型
-model = joblib.load(r"F:\小论文8\机器学习\模型2\RandomForest.joblib")
 
 # 读取数据（假设与训练模型时相同的数据）
 data = pd.read_excel(r"F:\小论文8\机器学习2\数据\2.xlsx")
